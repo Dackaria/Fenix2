302a250918a5d86323ce82cd1b34b502ffddb868
Dackaria <caelesti_dreams@hotmail.com>
2024-03-14 10:18:40 -0300

    navigate

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
 import CartWidget from "../cartWidget/CartWidget";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div className="flex justify-between items-center px-8 py-2 bg-gray-800 w-full" >
            <div className="flex items-center w-full">
-              <h1 className="mr-9 ml-8  text-white">Fenix</h1>
+              <h1 className="mr-9 ml-8  text-white" onClick={()=> navigate('/')}>Fenix</h1>
               <nav className="flex space-x-4">
                 <Link to='/category/mochila' className="hover:text-blue-500 px-8 py-8">Mochilas</Link>
                 <Link to='/category/cartera' className="hover:text-blue-500 px-8 py-8">Carteras</Link>
